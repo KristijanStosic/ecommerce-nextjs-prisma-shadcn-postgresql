791615ea8f222b00306d1dca9bfff784d09a4fd8
kristijanstosic <kristijan.stosic@lilly021.com>
2024-08-12 14:01:19 +0200

    Initial project

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Link from "next/link";
+import PageHeader from "../_components/PageHeader";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">
+            <Plus size={16} className="mr-1" /> Add Product
+          </Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        
+      </TableBody>
+    </Table>
+  );
+}
