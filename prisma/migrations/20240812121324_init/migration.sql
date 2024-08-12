6051c75840016e08ebdd0946e37b988736646a46
kristijanstosic <kristijan.stosic@lilly021.com>
2024-08-12 14:17:50 +0200

    Modified products table and ran migration

--- prisma/migrations/20240812121324_init/migration.sql
+++ prisma/migrations/20240812121324_init/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE "Product" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "priceInCents" INTEGER NOT NULL,
-    "countInStock" INTEGER NOT NULL,
     "filePath" TEXT NOT NULL,
     "imagePath" TEXT NOT NULL,
     "description" TEXT NOT NULL,
